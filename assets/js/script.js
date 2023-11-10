c94ffb2069cc3392bb09a415c71867927fdb34d3
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2023-11-10 18:42:21 +0100

    caroussel is working perfectly

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,62 +41,69 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
 $(document).ready(function () {
-function moveToSelected(element) {
+    function moveToSelected(element) {
 
-    if (element == "next") {
-      var selected = $(".selected").next();
-    } else if (element == "prev") {
-      var selected = $(".selected").prev();
-    } else {
-      var selected = element;
+        if (element == "next") {
+            var selected = $(".selected").next();
+        } else if (element == "prev") {
+            var selected = $(".selected").prev();
+        } else {
+            var selected = element;
+        }
+
+        var next = $(selected).next();
+        var prev = $(selected).prev();
+        var prevSecond = $(prev).prev();
+        var nextSecond = $(next).next();
+
+        $(selected).removeClass().addClass("selected");
+
+        $(prev).removeClass().addClass("prev");
+        $(next).removeClass().addClass("next");
+
+        $(nextSecond).removeClass().addClass("nextRightSecond");
+        $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+        $(nextSecond).nextAll().removeClass().addClass('hideRight');
+        $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
     }
-  
-    var next = $(selected).next();
-    var prev = $(selected).prev();
-    var prevSecond = $(prev).prev();
-    var nextSecond = $(next).next();
-  
-    $(selected).removeClass().addClass("selected");
-  
-    $(prev).removeClass().addClass("prev");
-    $(next).removeClass().addClass("next");
-  
-    $(nextSecond).removeClass().addClass("nextRightSecond");
-    $(prevSecond).removeClass().addClass("prevLeftSecond");
-  
-    $(nextSecond).nextAll().removeClass().addClass('hideRight');
-    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-  
-  }
-  
-  // Eventos teclado
-  $(document).keydown(function(e) {
-      switch(e.which) {
-          case 37: // left
-          moveToSelected('prev');
-          break;
-  
-          case 39: // right
-          moveToSelected('next');
-          break;
-  
-          default: return;
-      }
-      e.preventDefault();
-  });
-  
-  $('#carousel div').click(function () {
-    moveToSelected($(this));
-  });
 
-  $('#prev').click(function () {
-    moveToSelected('prev');
-  });
+    $(document).keydown(function (e) {
+        switch (e.which) {
+            case 37: // left
+                moveToSelected('prev');
+                break;
 
-  $('#next').click(function () {
-    moveToSelected('next');
-  });
+            case 39: // right
+                moveToSelected('next');
+                break;
+
+            default: return;
+        }
+        e.preventDefault();
+    });
+
+    $('#carousel div').click(function () {
+        moveToSelected($(this));
+    });
+
+    $('#prev').click(function () {
+        moveToSelected('prev');
+    });
+
+    $('#next').click(function () {
+        moveToSelected('next');
+    });
+
+
+    // Gestionnaire d'événements
+    $('.projet_caroussel-link').on('click', function (e) {
+        // Si la div parente n'a pas la classe "selected", annule le comportement par défaut du clic
+        if (!$(this).parent().hasClass('selected')) {
+            e.preventDefault();
+        }
+    });
+
 });
