21d6cf48af070e61b61e51d8938d56135b487c87
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2023-11-10 15:09:34 +0100

    added a carroussel + changed the header image

---
+++ assets/js/script.js
@@ -0,0 +1,102 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.getElementById("myCarousel");
+    let currentIndex = 0;
+    let intervalId; // Variable pour stocker l'ID de l'intervalle
+
+    function showItem(index) {
+        const translateValue = -index * 20; // 20% per item
+        carousel.style.transform = `translateX(${translateValue}%)`;
+    }
+
+    function nextItem() {
+        currentIndex = (currentIndex + 1) % 5; // Assuming 5 items
+        showItem(currentIndex);
+        resetInterval();
+    }
+
+    function prevItem() {
+        currentIndex = (currentIndex - 1 + 5) % 5; // Assuming 5 items
+        showItem(currentIndex);
+        resetInterval();
+    }
+
+    function resetInterval() {
+        clearInterval(intervalId);
+        intervalId = setInterval(nextItem, 3000);
+    }
+
+    intervalId = setInterval(nextItem, 3000); // Auto advance every 3 seconds
+
+    // Ajouter des gestionnaires d'événements pour les boutons
+    const nextButton = document.getElementById("nextButton");
+    const prevButton = document.getElementById("prevButton");
+
+    nextButton.addEventListener("click", function () {
+        nextItem();
+    });
+
+    prevButton.addEventListener("click", function () {
+        prevItem();
+    });
+});
+
+
+
+
+$(document).ready(function () {
+function moveToSelected(element) {
+
+    if (element == "next") {
+      var selected = $(".selected").next();
+    } else if (element == "prev") {
+      var selected = $(".selected").prev();
+    } else {
+      var selected = element;
+    }
+  
+    var next = $(selected).next();
+    var prev = $(selected).prev();
+    var prevSecond = $(prev).prev();
+    var nextSecond = $(next).next();
+  
+    $(selected).removeClass().addClass("selected");
+  
+    $(prev).removeClass().addClass("prev");
+    $(next).removeClass().addClass("next");
+  
+    $(nextSecond).removeClass().addClass("nextRightSecond");
+    $(prevSecond).removeClass().addClass("prevLeftSecond");
+  
+    $(nextSecond).nextAll().removeClass().addClass('hideRight');
+    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+  
+  }
+  
+  // Eventos teclado
+  $(document).keydown(function(e) {
+      switch(e.which) {
+          case 37: // left
+          moveToSelected('prev');
+          break;
+  
+          case 39: // right
+          moveToSelected('next');
+          break;
+  
+          default: return;
+      }
+      e.preventDefault();
+  });
+  
+  $('#carousel div').click(function () {
+    moveToSelected($(this));
+  });
+
+  $('#prev').click(function () {
+    moveToSelected('prev');
+  });
+
+  $('#next').click(function () {
+    moveToSelected('next');
+  });
+});
