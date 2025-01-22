c760738d62f83c48ec7eee1f86af6fc9afa25235
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2025-01-22 20:38:42 +0100

    have to start again the mobile version

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,11 +118,5 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector('.navigation');
-
-menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
-
+  
 });
