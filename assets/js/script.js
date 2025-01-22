abca876dc614760e5a2c482693a1c1024bad61b6
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2025-01-22 20:21:54 +0100

    test mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,4 +117,12 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+  const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.navigation');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
 });
